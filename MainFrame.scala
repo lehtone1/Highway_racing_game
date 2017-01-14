d43de679b9c9829457df0e58060093c47d712fe7
lehtone1 <eero.lehtonen@aalto.fi>
2017-01-14 20:26:22 +0200

    No bugging

--- MainFrame.scala
+++ MainFrame.scala
@@ -1,14 +1,13 @@
-package game
+package o1.game
 import scala.swing._
 import scala.swing.event._
 import java.awt.Color
 import java.awt.event._
 import java.awt.{geom}
-import game.Wall
-import game.Spot
-import game.Player
-import game.Floor
-import game.Timer
+import o1.game.Wall
+import o1.game.Spot
+import o1.game.Player
+import o1.game.Floor
 import scala.collection.mutable.Buffer
 
 import java.io.File
@@ -30,41 +29,36 @@ import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 import javax.swing.Timer;
 
-object MainFrame extends SimpleSwingApplication  {
-  //val background = game.BackgroundAnimation
-  //val flowpanel = background.canvas
-  val width = 40
-  val height = 20
-  val cellSize = 25
-  var world: Array[Array[Spot]] = Array.fill(width, height)(Floor)
+object MainFrame2 extends SimpleSwingApplication {
+  val squareSize = 6
+  val width = 250
+  val height = 125
+  val cellSize = 4
+  var world: Array[Array[Spot]] = Array.fill(height, width)(Floor)
+  var firstSquares: Array[Array[Spot]] = Array.fill(squareSize,width )(Wall)
   val r = scala.util.Random
-  val player = new Player(width/2, height/2)
+  val player = new Player(0, 0)
   var pressedKeys = Buffer[Key.Value]() //A buffer that holds all keys that are pressed at the same time
   var trueFalseTable = Buffer[Boolean]()
   
-
+  
   // Moves everything in the world one space downward
   def moveWorldDown = {
-    var row = 0
-    player.move(player.x, player.y + 1)
-    while(row < width){
-      var line = world(0).size
-      while(line >= 0){
-        if(line < height - 1) {
-          world(row)(line + 1) = world(row)(line)
-        }
-        line -= 1
-      }
-      row += 1
+    var line = height - 1
+    player.move(player.x, player.y + cellSize)
+    while(line > 0){
+      world(line) = world(line - 1)
+      line -=1
     }
+      
   }
   
-  //Makes a true false array where the value is true if there is a wall and false if there is a wall
+   //Makes a true false array where the value is true if there is a wall and false if there is a wall
   def makeTrueFalseArray = {
     trueFalseTable = Buffer[Boolean]()
     var row = 0
     while(row < width){
-       if((world(row)(1) == Wall && world(row)(2) == Wall) || (world(row)(2) == Wall && world(row)(3) == Wall) || (world(row)(3) == Wall && world(row)(4) == Wall)  ){
+       if((world(1)(row) == Wall && world(1 + squareSize)(row) == Wall) || (world(1 + squareSize)(row) == Wall && world(1 + 2 * squareSize)(row) == Wall) || (world(1 + 2 * squareSize)(row) == Wall && world(1 + 3 * squareSize)(row) == Wall)  ){
           trueFalseTable += false
        }else{
          trueFalseTable += true
@@ -74,41 +68,36 @@ object MainFrame extends SimpleSwingApplication  {
   }
   
   //Checks if the first line of holed wall is acceptable and does not cause an impassable line
-  //WORK IN PROGRES
   def checkIfAcceptable = {
     var acceptable = false
     for(number <- 0 to width -1 ){
-      if(trueFalseTable(number) && world(number)(0) == Floor){
+      if(trueFalseTable(number) && world(1)(number) == Floor){
         acceptable = true
       }
     }
+    
     acceptable
   }
   
-  //Creates a line of walls and floors
+   //Creates a line of walls and floors
   def createHoledLine = {
-  
-    //Creates a row full of wall objects
-    def createWall(line: Int) = {
-      world.map(_(line) = Wall)
-    }
-    
-    //Creates holes marked with Floor-objects to the row.
-    def createHoles(line: Int) = {
-      do{ 
-        createWall(0)
-        val randomNum =  10 + r.nextInt(width  - 10)
+      
+      do{
+        println("loop")
+        val randomNum = 8 + r.nextInt(width/squareSize - 8)
         var crawler = 0
         while(crawler < randomNum){
-          val randomNum2 = r.nextInt(width - 1)
-          world(randomNum2)(line) = Floor
+          var randomNumTreeni = 1 +  r.nextInt(width/squareSize - 1)
+          var randomNum2 = (squareSize * randomNumTreeni)
+          var squareLowerLimit = randomNum2- squareSize
+          for(line <- 0  until squareSize){
+            for(place <- squareLowerLimit until randomNum2){
+              firstSquares(line)(place) = Floor
+            }
+          }
           crawler += 1
           }   
       }while(checkIfAcceptable == false)
-      
-      
-    }
-    createHoles(0)
   
   }
   
@@ -133,7 +122,7 @@ object MainFrame extends SimpleSwingApplication  {
   var myColor = new Color(255,255,255,0)
   var srcy1a=0-FRAME_H
   var srcy2a=0
-  var speed = 10
+  var speed = 1
   var INCREMENT = 1 // määrittää nopeuden jolla taustakuva liikkuu
                      // saman tekee myös timer arvo
                      // voidaan esimerkiksi tehdä niin, että lisätään moveBackground-metodiin
@@ -141,50 +130,44 @@ object MainFrame extends SimpleSwingApplication  {
                      // timer on jaollinen 50 niin vauhti kasvaa 5 !ESIM!:p
     
   
-  val canvas = new GridPanel(rows0 = height, cols0 = width) {
+  
+  
+  val canvas = new GridPanel(rows0 = width, cols0 = height) {
+
     
   preferredSize = new Dimension(width * cellSize, height * cellSize)
   
-  
-
-  
   //Paints the world again after some event
   
-  override def paintComponent(g: Graphics2D) {
-      g.drawImage(background_IMG, dx1, dy1, dx2, dy2, srcx1, srcy2,
-                    srcx2, srcy1, null);
-      g.drawImage(background_IMG, dx1, dy1, dx2, dy2, srcx1, srcy2a,srcx2, srcy1a, null);
-      
-      g.drawString(progress_in_meters.toString()+"m", 10, 10)
-      for (i <- 0 until width) {
-        for (k <- 0 until height) { // Loop through the world grid
-          world(i)(k) match {       // Match what is found in every position
-            case Wall => {          // If a wall is there, change color to black and paint a black tile representing a wall
-              g.setColor(Color.BLACK)
-              g.fillRect(i * cellSize, k * cellSize, cellSize, cellSize)
-            }
-            case Floor => {         // If a floor is there, change color to cyan and paint a cyan tile representing floor
-              g.setColor(myColor)
-              g.fillRect(i * cellSize, k * cellSize, cellSize, cellSize)
+  
+  
+    override def paintComponent(g: Graphics2D) {
+        g.drawImage(background_IMG, dx1, dy1, dx2, dy2, srcx1, srcy2,
+                      srcx2, srcy1, null);
+        g.drawImage(background_IMG, dx1, dy1, dx2, dy2, srcx1, srcy2a,srcx2, srcy1a, null);
+        
+        g.drawString(progress_in_meters.toString()+"m", 10, 10)
+        for (i <- 0 until width) {
+          for (k <- 0 until height) { // Loop through the world grid
+            world(k)(i) match {       // Match what is found in every position
+              case Wall => {          // If a wall is there, change color to black and paint a black tile representing a wall
+                g.setColor(Color.BLACK)
+                g.fillRect(i * cellSize, k * cellSize, cellSize, cellSize)
+              }
+              case Floor => {         // If a floor is there, change color to cyan and paint a cyan tile representing floor
+                g.setColor(myColor)
+                g.fillRect(i * cellSize, k * cellSize, cellSize, cellSize)
+              }
             }
           }
         }
+        g.setColor(Color.ORANGE)      // Set color for the player to be drawn
+        g.fillOval(player.x, player.y, squareSize * cellSize, squareSize * cellSize) // Draw player to its location
+        g.setColor(Color.GRAY)
+        
+        
       }
-      g.setColor(Color.ORANGE)      // Set color for the player to be drawn
-      g.fillOval(player.x * cellSize, player.y * cellSize, cellSize, cellSize) // Draw player to its location
-      g.setColor(Color.GRAY)
-      
-      
-    }
   
-  val timer = new javax.swing.Timer(speed,Swing.ActionListener { e =>
-      
-      moveBackground()
-      repaint()
-      progress_in_meters+=1
-      })
-    timer.start()
-    
     def moveBackground(){
       if (srcy1 == FRAME_H) {
         srcy1 = 1-FRAME_H
@@ -211,7 +194,6 @@ object MainFrame extends SimpleSwingApplication  {
     }
   }
   
-  
   def top = new MainFrame {
     title = "Game"
     preferredSize = new Dimension(width * cellSize, height * cellSize)
@@ -223,17 +205,30 @@ object MainFrame extends SimpleSwingApplication  {
     canvas.focusable = true
     canvas.requestFocus
     
-    var timeInterval = 1500
+    var timeInterval = 150
+    var firstSquaresCrawler = 0
     
     //Creates Timer and the timer event
     val timeOut = new javax.swing.AbstractAction() {
       def actionPerformed(e : java.awt.event.ActionEvent) ={
+        if(firstSquaresCrawler == 0){
+          firstSquares = Array.fill(height,width )(Wall)
+          makeTrueFalseArray
+          createHoledLine
+        }
+        canvas.moveBackground()
+        repaint()
+        progress_in_meters+=1
         moveWorldDown
-        makeTrueFalseArray
-        createHoledLine
+        world(0) = firstSquares(firstSquaresCrawler)
+        firstSquaresCrawler += 1
+        if(firstSquaresCrawler == squareSize){
+          firstSquaresCrawler = 0
+        }
         repaint()
       }
     }
+    
     val t = new javax.swing.Timer(timeInterval, timeOut)
     t.setRepeats(true)
     t.start()
@@ -275,7 +270,7 @@ object MainFrame extends SimpleSwingApplication  {
     def highScore() {
       Dialog.showMessage(contents.head, "Thank you!", title="High score")
     }
-
+    
     //Gane menu functionalities About, High Score and Exit
     menuBar = new MenuBar {   
         contents += new Menu("Menu") {      
@@ -286,60 +281,57 @@ object MainFrame extends SimpleSwingApplication  {
           contents += new MenuItem(Action("Exit") { dispose() })  
         }
     }
+    
     //Determines the game reactions for keyboards and mouse events
     reactions += {
       case KeyPressed(canvas, key, _, _) => { //When any key is pressed
-        timeInterval -= 2 //Decreases the Time Interval between events
+        println(player.x + " * " + player.y)
+        timeInterval -= 1 //Decreases the Time Interval between events
         t.setDelay(timeInterval) //Sets the new time interval to the Timer
         pressedKeys += key //the key is added to the pressed keys
         if(pressedKeys contains Key.Space){ //When space is one of the keys
           if(pressedKeys contains Key.Left){//the player moves two spaces to 
-            if(world(player.x - 2)(player.y) != Wall){//chosen direction
-              player.move(player.x - 2, player.y)
+            if(world(player.y / cellSize)(player.x / cellSize - squareSize * 2) != Wall){//chosen direction
+              player.move(player.x - squareSize * 2 * cellSize , player.y)
               repaint()
             }
           }else if(pressedKeys contains Key.Right) {
-            if(world(player.x + 2)(player.y) != Wall){
-              player.move(player.x + 2, player.y)
+            if(world(player.y/ cellSize)(player.x/ cellSize + squareSize * 2 ) != Wall){
+              player.move(player.x + squareSize * 2 * cellSize, player.y)
               repaint()
             }
           }else if(pressedKeys contains Key.Up) {
-            if(world(player.x)(player.y - 2) != Wall){
-              player.move(player.x, player.y - 2)
+            if(world(player.y/ cellSize - squareSize * 2)(player.x/ cellSize) != Wall){
+              player.move(player.x, player.y - squareSize * 2 * cellSize)
               repaint()
             }
           }else if(pressedKeys contains Key.Down){
-            if(world(player.x)(player.y + 2) != Wall){
-              player.move(player.x, player.y + 2)
+            if(world(player.y/ cellSize + squareSize * 2)(player.x/ cellSize) != Wall){
+              player.move(player.x, player.y + squareSize * 2 * cellSize)
               repaint()
             }
           }   
         }else if(pressedKeys contains Key.Left){
-          if(world(player.x - 1)(player.y) != Wall){
-            player.move(player.x - 1, player.y)
+          if(world(player.y/ cellSize)(player.x/ cellSize - squareSize) != Wall){
+            player.move(player.x - squareSize * cellSize, player.y)
             repaint()
             }
         }else if(pressedKeys contains Key.Right) {
-          if(world(player.x + 1)(player.y) != Wall){
-            player.move(player.x + 1, player.y)
+          if(world(player.y/ cellSize)(player.x/ cellSize + squareSize ) != Wall){
+            player.move(player.x + squareSize * cellSize, player.y)
             repaint()
             }
         }else if(pressedKeys contains Key.Up) {
-          if(world(player.x)(player.y - 1) != Wall){
-            player.move(player.x, player.y - 1)
+          if(world(player.y/ cellSize - squareSize)(player.x/ cellSize) != Wall){
+            player.move(player.x, player.y - squareSize * cellSize)
             repaint()
             }
         }else if(pressedKeys contains Key.Down){
-          if(world(player.x)(player.y + 1) != Wall){
-            player.move(player.x, player.y + 1)
+          if(world(player.y/ cellSize + squareSize)(player.x/ cellSize) != Wall){
+            player.move(player.x, player.y + squareSize * cellSize)
             repaint()
           }
-        }else if(pressedKeys contains Key.Enter){ //moves the world downwards and creates a new line to the first line
-          moveWorldDown
-          makeTrueFalseArray
-          createHoledLine
-          repaint()
-          }
+        }
       }
       
           
@@ -348,16 +340,11 @@ object MainFrame extends SimpleSwingApplication  {
         pressedKeys = Buffer[Key.Value]()
       }
        
-      case MouseClicked(canvas, point, _, clicks, _) => {  
-        if(clicks == 2){
-          world(point.x/cellSize)(point.y/cellSize) = Floor
-          repaint()
-        }else{
-          world(point.x/cellSize)(point.y/cellSize) = Wall
-          repaint()
-        }
-      }
     }
 
   }
 }
+  
+  
+  
+  
