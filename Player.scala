7746cb568e5b2bb5f4f3e20799261484a018b50b
Pöyhiä Sakari <sakari.poyhia@aalto.fi>
2017-01-18 21:49:55 +0200

    player

--- Player.scala
+++ Player.scala
@@ -1,16 +1,20 @@
-package o1.game
+package game
 
 class Player(var x: Int, var y: Int) {
-  
   def move(newX: Int, newY: Int) = {
     x = newX
     y = newY
   }
 
-  def canMoveTo(xPos: Int, yPos: Int) = {
-    Math.abs(xPos - x) <= 1 && Math.abs(yPos - y) <= 1
+  def canMoveTo(xPos: Int, yPos: Int)= {
+    //Math.abs(xPos - x) <= 10 && Math.abs(yPos - y) <= 10
+    !((xPos < 0 || xPos >= game.MainFrame2.width*game.MainFrame2.squareSize)|| (yPos < 0 || yPos > game.MainFrame2.height*game.MainFrame2.squareSize))
   }
   
+  def jumpCanMoveTo(xPos: Int, yPos: Int)= {
+    //Math.abs(xPos - x) <= 10 && Math.abs(yPos - y) <= 10
+    !((xPos < 0 || xPos >= game.MainFrame2.width*game.MainFrame2.squareSize)|| (yPos < 0 || yPos > game.MainFrame2.height*game.MainFrame2.squareSize))
+  }
   
 
 }
